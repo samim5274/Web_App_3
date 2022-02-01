a397a019cdb2b31c791c563a8bf660ec6d544c8e
Samim Hossen <banglarcele1122@gmail.com>
2022-02-01 14:32:41 +0600

    Up

---
+++ WebApp3/WebApp3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApp3.MvcApplication" Language="C#" %>
---
+++ WebApp3/WebApp3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp3.Startup))]
+namespace WebApp3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
